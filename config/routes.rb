a98dfca99bbc81579b6f6485f3b18e4536c02f3d
Rachel Sterneck <rachel.sterneck@gmail.com>
2017-05-04 01:34:51 -0400

    Tagging feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles do
-  	resources :comments
-  end
-  root to: 'articles#index'
+
+	root to: 'articles#index'
+  	resources :articles do
+  		resources :comments
+  	end
+  	resources :tags
+
 end
